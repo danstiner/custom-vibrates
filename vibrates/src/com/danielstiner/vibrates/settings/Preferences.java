48daff8e4d388cfa12a7d37e4209450445f3f175
Daniel Stiner <daniel.stiner@gmail.com>
2012-04-23 22:00:57 -0500

    Preferences has working home button

--- vibrates/src/com/danielstiner/vibrates/settings/Preferences.java
+++ vibrates/src/com/danielstiner/vibrates/settings/Preferences.java
@@ -4,8 +4,10 @@ import roboguice.activity.RoboPreferenceActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.danielstiner.vibrates.R;
+import com.danielstiner.vibrates.view.EntitiesActivity;
 
 public class Preferences extends RoboPreferenceActivity {
 
@@ -13,6 +15,19 @@ public class Preferences extends RoboPreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		getActionBar().setHomeButtonEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			EntitiesActivity.show(this);
+		}
+		
+		return super.onOptionsItemSelected(item);
 	}
 
 	public static void show(Context context) {
