cd2541416ea37ff3f50d830d67fb71c35fba2719
Daniel Stiner <daniel.stiner@gmail.com>
2012-04-23 21:59:40 -0500

    Added Facebook, gTalk and gmail types to accessibility service

--- vibrates/src/com/danielstiner/vibrates/notify/AccessibilityHandler.java
+++ vibrates/src/com/danielstiner/vibrates/notify/AccessibilityHandler.java
@@ -14,12 +14,17 @@ public class AccessibilityHandler {
 	private static final String GOOGLE_VOICE_PACKAGE = "com.google.android.apps.googlevoice";
 	private static final String GOOGLE_VOICE_TYPE = NotificationTypes
 			.particularizeType(NotificationTypes.CHAT_SMS, "google_voice");
-	
-	private static final String FACEBOOK_PACKAGE = "com.google.android.apps.googlevoice";
+
+	private static final String FACEBOOK_PACKAGE = "com.facebook.orca";
 	private static final String FACEBOOK_TYPE = NotificationTypes
-			.particularizeType(NotificationTypes.CHAT, "facebook");
+			.particularizeType(NotificationTypes.CHAT_IM, "facebook");
+
+	private static final String GOOGLE_TALK_PACKAGE = "com.google.android.gsf";
+	private static final String GOOGLE_TALK_TYPE = NotificationTypes
+			.particularizeType(NotificationTypes.CHAT_IM, "google_talk");
 
 	private static final String GOOGLE_GMAIL_PACKAGE = "com.google.android.gm";
+	private static final String GOOGLE_GMAIL_TYPE = NotificationTypes.MESSAGE_EMAIL;
 
 	private Provider<VibrateNotify> notify_provider;
 
@@ -36,31 +41,25 @@ public class AccessibilityHandler {
 		String packageName = event.getPackageName().toString();
 
 		if (packageName.equals(GOOGLE_VOICE_PACKAGE))
-			handleGoogleVoice(event, context);
+			handleChat(event, context, GOOGLE_VOICE_TYPE);
 		else if (packageName.equals(GOOGLE_GMAIL_PACKAGE))
 			handleGmail(event, context);
 		else if (packageName.equals(FACEBOOK_PACKAGE))
-			handleFacebook(event, context);
+			handleChat(event, context, FACEBOOK_TYPE);
+		else if (packageName.equals(GOOGLE_TALK_PACKAGE))
+			handleChat(event, context, GOOGLE_TALK_TYPE);
 	}
 
-	private void handleGoogleVoice(AccessibilityEvent event, Context context) {
+	private void handleChat(AccessibilityEvent event, Context context,
+			String chat_type) {
 
 		VibrateNotify n = chatNotification(event);
-		
-		if(n != null)
-			n.source_type(GOOGLE_VOICE_TYPE).fire(context);
+
+		if (n != null)
+			n.source_type(chat_type).fire(context);
 	}
-	
-	private void handleFacebook(AccessibilityEvent event, Context context) {
-		
-		VibrateNotify n = chatNotification(event);
-		
-		if(n != null)
-			n.source_type(FACEBOOK_TYPE).fire(context);
-	}
-	
-	private VibrateNotify chatNotification(AccessibilityEvent event)
-	{
+
+	private VibrateNotify chatNotification(AccessibilityEvent event) {
 		if (event.getText().size() < 1) {
 			Ln.e("Bad chat notifcation: no event text!");
 			return null;
@@ -68,20 +67,24 @@ public class AccessibilityHandler {
 
 		// Split the notification up
 		String[] message_parts = event.getText().get(0).toString()
-				.split(":", 2);
+				.split(": ", 2);
 		if (message_parts.length != 2) {
 			Ln.e("Bad chat notifcation: no colon separated name:message!");
 			return null;
 		}
 
 		String name = message_parts[0].trim();
-		String text = message_parts[1].trim();
+		String text = message_parts[1];
 		return notify_provider.get().identifier(name, Identifiers.Common.Name)
 				.extra(text);
 	}
 
 	private void handleGmail(AccessibilityEvent event, Context context) {
 
+		String name = event.getText().get(0).toString();
+
+		notify_provider.get().identifier(name, Identifiers.Common.Name)
+				.source_type(GOOGLE_GMAIL_TYPE).fire(context);
 	}
 
 }
