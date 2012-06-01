5e0da0551f0c7132ef4071640d506fe790d44084
Daniel Stiner <daniel.stiner@gmail.com>
2012-06-01 02:37:00 +0100

    Launch activity now exists when reached from a back button

--- custom-vibrates/src/main/java/com/danielstiner/vibrates/view/LaunchActivity.java
+++ custom-vibrates/src/main/java/com/danielstiner/vibrates/view/LaunchActivity.java
@@ -5,13 +5,24 @@ import android.content.Context;
 import android.os.Bundle;
 
 public class LaunchActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// TODO On first launch, instead show the welcome screen
-		EntitiesActivity.show((Context) this);
+
+		// Show contact view if its the launch of application
+		// Otherwise assume the back button was pressed and exit
+		if (savedInstanceState == null)
+			EntitiesActivity.show((Context) this);
+		else
+			finish();
+	}
+	
+	@Override
+	protected void onResume() {
+		finish();
 	}
 
 }
